b65e59109dc1c3c7b7fec3169666bf344d148a3f
ghcchina <hongchao_guan@apple.com>
2023-12-27 22:03:49 -0800

    https://pacmancode.com/spawn-mode

--- original/pacman.py
+++ original/pacman.py
@@ -3,23 +3,14 @@ from pygame.locals import *
 from vector import Vector2
 from nodes import Node
 from constants import *
+from entity import Entity
 
-class Pacman(object):
+
+class Pacman(Entity):
     def __init__(self, node):
+        Entity.__init__(self, node)
         self.name = PACMAN
-        # self.position = Vector2(200, 400)
-        self.directions = {STOP:Vector2(), UP:Vector2(0,-1), DOWN:Vector2(0,1), LEFT:Vector2(-1,0), RIGHT:Vector2(1,0)}
-        self.direction = STOP
-        self.speed = 100 * TILEWIDTH/16
-        self.radius = 10
         self.color = YELLOW
-        self.node = node
-        self.setPosition()
-        self.target = node
-        self.collideRadius = 5
-
-    def setPosition(self):
-        self.position = self.node.position.copy()
 
     def update(self, dt):	
         self.position += self.directions[self.direction] * self.speed * dt
@@ -46,17 +37,6 @@ class Pacman(object):
             if self.oppositeDirection(direction):
                 self.reverseDirection()
 
-    def validDirection(self, direction) -> bool:
-        if direction is not STOP:
-            if self.node.neighbors[direction] is not None:
-                return True
-        return False
-
-    def getNewTarget(self, direction) -> Node:
-        if self.validDirection(direction):
-            return self.node.neighbors[direction]
-        return self.node
-
     def getValidKey(self) -> int:
         key_pressed = pygame.key.get_pressed()
         if key_pressed[K_UP]:
@@ -68,38 +48,20 @@ class Pacman(object):
         if key_pressed[K_RIGHT]:
             return RIGHT
         return STOP
-
-    def render(self, screen):
-        p = self.position.asInt()
-        pygame.draw.circle(screen, self.color, p, self.radius)
-
-    def overshotTarget(self):
-        if self.target is not None:
-            vec1 = self.target.position - self.node.position
-            vec2 = self.position - self.node.position
-            node2Target = vec1.magnitudeSquared()
-            node2Self = vec2.magnitudeSquared()
-            return node2Self >= node2Target
-        return False
-    
-    def reverseDirection(self):
-        self.direction *= -1
-        temp = self.node
-        self.node = self.target
-        self.target = temp
-
-    def oppositeDirection(self, direction):
-        if direction is not STOP:
-            if direction == self.direction * -1:
-                return True
-        return False
     
     def eatPellets(self, pelletList):
         for pellet in pelletList:
-            d = self.position - pellet.position
-            dSquared = d.magnitudeSquared()
-            rSquared = (pellet.radius+self.collideRadius)**2
-            if dSquared <= rSquared:
+            if self.collideCheck(pellet):
                 return pellet
         return None
-    
+
+    def collideGhost(self, ghost):
+        return self.collideCheck(ghost)
+
+    def collideCheck(self, other):
+        d = self.position - other.position
+        dSquared = d.magnitudeSquared()
+        rSquared = (self.collideRadius + other.collideRadius)**2
+        if dSquared <= rSquared:
+            return True
+        return False    
--- original/run.py
+++ original/run.py
@@ -4,6 +4,7 @@ from constants import *
 from pacman import Pacman
 from nodes import NodeGroup
 from pellets import PelletGroup
+from ghosts import Ghost
 
 class GameController(object):
     def __init__(self):
@@ -22,17 +23,29 @@ class GameController(object):
         self.nodes = NodeGroup("maze1.txt")
         self.nodes.setPortalPair((0,17), (27,17))
         # self.nodes.setupTestNodes()
+        homekey = self.nodes.createHomeNodes(11.5, 14)
+        self.nodes.connectHomeNodes(homekey, (12,14), LEFT)
+        self.nodes.connectHomeNodes(homekey, (15,14), RIGHT)
         # self.pacman = Pacman(self.nodes.nodeList[0])
         self.pacman = Pacman(self.nodes.getStartTempNode())
         self.pellets = PelletGroup("maze1.txt")
+        self.ghost = Ghost(self.nodes.getStartTempNode(), self.pacman)
+        self.ghost.setSpawnNode(self.nodes.getNodeFromTiles(2+11.5, 3+14))
 
     def update(self):
         dt = self.clock.tick(30) / 1000.0
         self.pacman.update(dt)
+        self.ghost.update(dt)
         self.pellets.update(dt)
         self.checkPelletEvents()
+        self.checkGhostEvents()
         self.checkEvents()
         self.render()
+
+    def checkGhostEvents(self):
+        if self.pacman.collideGhost(self.ghost):
+            if self.ghost.mode.current is FREIGHT:
+               self.ghost.startSpawn()
 
     def checkEvents(self):
         for event in pygame.event.get():
@@ -44,6 +57,7 @@ class GameController(object):
         self.nodes.render(self.screen)
         self.pellets.render(self.screen)
         self.pacman.render(self.screen)
+        self.ghost.render(self.screen)
         pygame.display.update()  
 
     def checkPelletEvents(self):
@@ -51,6 +65,8 @@ class GameController(object):
         if pellet:
             self.pellets.numEaten += 1
             self.pellets.pelletList.remove(pellet)
+            if pellet.name == POWERPELLET:
+               self.ghost.startFreight()            
 
 if __name__ == "__main__":
     game = GameController()
